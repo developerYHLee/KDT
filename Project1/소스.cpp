c87f2f8fa69364fc3e98a0559acb1b6d5c384314
developerYHLee <dbsgh812@naver.com>
2023-03-26 14:22:31 +0900

    1주차 C++ 4번째 수업
    vector 및 namespace

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -1,48 +1,84 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
-int add(int a, int b);
-int sub(int a, int b);
-int multiple(int a, int b);
-float divide(int a, int b);
-void isOdd(int num);
-int findMax(int, int, int);
 using namespace std;
+namespace Seoul {
+	string number, landmark;
+}
 
+namespace Busan {
+	string number, landmark;
+}
+
+using namespace Busan;
 int main() {
-	int a = 7, b = 12, c = -7;
+	Seoul::number = "02";
+	Seoul::landmark = "광화문";
+	number = "051";
+	landmark = "광안리";
+	cout << "서울 지역번호와 랜드마크 출력 : " << Seoul::number << ' ' << Seoul::landmark << endl;
+	cout << "부산 지역번호와 랜드마크 출력 : " << number << ' ' << landmark << endl << endl;
 
-	cout << "a : " << a << " b : " << b << '\n';
-	cout << "add : " << add(a, b) << '\n';
-	cout << "minus : " << sub(a, b) << '\n';
-	cout << "multiple : " << multiple(a, b) << '\n';
-	cout << "divide : " << divide(a, b) << '\n';
-	isOdd(a);
-	cout << a << "와 " << b << "와 " << c << " 중 가장 큰 수 : " << findMax(a, b, c);
+
+	bool vis[26] = { false };
+	vector<char> alpha;
+	cout << "입력 예시 : ";
+	string str1;
+	getline(cin, str1);
+
+	for (int i = 0; i < str1.length(); i++) {
+		int index = str1[i] - 'a';
+
+		if (index >= 0 && index < 26 && !vis[index]) {
+			vis[index] = true;
+			alpha.push_back(str1[i]);
+		}
+	}
+
+	int sum4 = 0;
+	for (int i = 0; i < 26; i++) {
+		if (vis[i]) sum4++;
+	}
+	cout << "출력예시 : " << sum4 << "개 (";
+	for (int i = 0; i < alpha.size() - 1; i++) cout << ' ' << alpha[i] << ',';
+	cout << ' ' << alpha[alpha.size() - 1] << ')' << endl << endl;
+
+	vector<int> v2;
+
+	cout << "사용자가 입력한 숫자 더하기" << endl << endl;
+	while (true) {
+		int num;
+		cout << "숫자를 입력하세요 (0 : exit) : ";
+		cin >> num;
+
+		if (num == 0) break;
+
+		v2.push_back(num);
+	}
+
+	int sum1 = 0;
+	for (int i : v2) sum1 += i;
+	cout << "사용자가 입력한 수의 합은 : " << sum1 << endl << endl;
+
+	int x, y;
+	while (true) {
+		cout << "x를 입력하세요 : ";
+		cin >> x;
+
+		cout << "y를 입력하세요 : ";
+		cin >> y;
+
+		if (x > 0 && y > 0) break;
+
+		cout << "x와 y 모두 양수를 입력해주세요." << endl;
+	}
+
+	vector<vector<int>> v1 = { {1,2,3,4},{5,6,7,8},{9,10,11,12} };
+	for (int i = 0; i < v1.size(); i++) {
+		for (int j = 0; j < v1[i].size(); j++) cout << v1[i][j] << ' ';
+		cout << endl;
+	}
+
 	return 0;
-}
-
-
-int add(int a, int b) {
-	return a + b;
-}
-
-int sub(int a, int b) {
-	return max(a, b) - min(a, b);
-}
-
-int multiple(int a, int b) {
-	return a * b;
-}
-
-float divide(int a, int b) {
-	return (float)a / b;
-}
-
-void isOdd(int num) {
-	if (num % 2 == 0) cout << num << "는(은) 짝수입니다.\n";
-	else cout << num << "는(은) 홀수입니다.\n";
-}
-
-int findMax(int a, int b, int c) {
-	return max(a, max(b, c));
 }
