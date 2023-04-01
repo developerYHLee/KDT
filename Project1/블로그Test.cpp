a9e29289869ddf3364affc164e32a4967021561b
developerYHLee <dbsgh812@naver.com>
2023-04-02 00:30:20 +0900

    2주차 블로그Test-1

--- Project1/블로그Test.cpp
+++ Project1/블로그Test.cpp
@@ -6,10 +6,14 @@ using namespace std;
 
 int main() {
 	ifstream file("hello.txt");
-	ofstream file2("bye.txt");
 
-	//if (!file) cout << "파일 열기 실패\n";
-	//if(file.fail()) cout << "파일 열기 실패\n";
+	ofstream file2;
+	file2.open("bye.txt"); //std::ios::app를 매개변수에 추가하면 데이터 이어쓰기 가능
+
+	file2 << "새로운 데이터 쓰기!" << endl;
+
+	if (!file) cout << "파일 열기 실패\n";
+	if(file.fail()) cout << "파일 열기 실패\n";
 	if(!file.is_open()) cout << "파일 열기 실패\n";
 	else {
 		cout << "파일 열기 성공\n";
@@ -19,6 +23,5 @@ int main() {
 	}
 
 	file.close();
-
-	
+	file2.close();
 }
