f5d1f1f99a172ca1a9113ce83f15404b92b7b595
developerYHLee <dbsgh812@naver.com>
2023-04-02 16:24:59 +0900

    2주차 C++ 5번쨰 수업

---
+++ Project1/Student.cpp
@@ -0,0 +1,5 @@
+#include "Student.h"
+
+Student::Student(string name) : Person(name) {
+		cout << "Student »ý¼ºÀÚ" << endl;
+}
--- Project1/블로그Test.cpp
+++ Project1/블로그Test.cpp
@@ -1,27 +1,21 @@
 #include <iostream>
-#include <fstream>
-#include <string>
 
 using namespace std;
 
-int main() {
-	ifstream file("hello.txt");
-
-	ofstream file2;
-	file2.open("bye.txt"); //std::ios::app를 매개변수에 추가하면 데이터 이어쓰기 가능
-
-	file2 << "새로운 데이터 쓰기!" << endl;
-
-	if (!file) cout << "파일 열기 실패\n";
-	if(file.fail()) cout << "파일 열기 실패\n";
-	if(!file.is_open()) cout << "파일 열기 실패\n";
-	else {
-		cout << "파일 열기 성공\n";
-	
-		string l;
-		while (getline(file, l)) file2 << l << endl;
+class Parent {
+public:
+	virtual void test() {
+		cout << "부모 클래스의 가상 함수 호출!" << endl;
 	}
+};
 
-	file.close();
-	file2.close();
+class Child : public Parent {
+	void test() {
+		cout << "자식 클래스의 가상 함수 호출!" << endl;
+	}
+};
+
+int main() {
+	Parent** child = new Parent*[3]{ new Child(), new Child() ,new Child() };
+	child[0]->test();
 }
